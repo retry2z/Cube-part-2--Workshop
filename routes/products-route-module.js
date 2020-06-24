205b6ec8ff4f8c8950369927e1668da811747b7c
Hristiyan Hristov <32390803+retry2z@users.noreply.github.com>
2020-06-24 08:36:55 +0300

    fix/owner-permission

--- routes/products-route-module.js
+++ routes/products-route-module.js
@@ -5,12 +5,17 @@ const cubeController = require('../controllers/cube-controller');
 //CREATE
 
 router.get('/create', async (req, res) => {
-    await cubeController.create.get(req, res);
+    const status = await cubeController.create.get(req, res);
+
+    if (!status) {
+        res.redirect('/error');
+    }
 });
 
 
 router.post('/create', async (req, res) => {
     const status = await cubeController.create.post(req, res);
+
     if (status) {
         res.redirect('/');
     }
@@ -20,7 +25,11 @@ router.post('/create', async (req, res) => {
 //LIST
 
 router.get('/list', async (req, res) => {
-    await cubeController.list.get(req, res);
+    const status = await cubeController.list.get(req, res);
+
+    if (!status) {
+        res.redirect('/error');
+    }
 });
 
 
@@ -42,18 +51,32 @@ router.post('/details/:id', async (req, res) => {
 //REMOVE
 
 router.get('/delete/:id', async (req, res) => {
-    await cubeController.delete.get(req, res);
+    const status = await cubeController.delete.get(req, res);
+
+    if (!status) {
+        res.redirect('/error');
+    }
 });
 
 router.post('/delete/:id', async (req, res) => {
-    await cubeController.delete.post(req, res);
-    res.redirect('/');
+    const status = await cubeController.delete.post(req, res);
+
+
+    if (status) {
+        res.redirect('/');
+    } else {
+        res.redirect('/error');
+    }
 });
 
 //EDIT
 
 router.get('/edit/:id', async (req, res) => {
-    await cubeController.edit.get(req, res);
+    const status = await cubeController.edit.get(req, res);
+
+    if (!status) {
+        res.redirect('/error');
+    }
 });
 
 router.post('/edit/:id', async (req, res) => {
@@ -61,6 +84,8 @@ router.post('/edit/:id', async (req, res) => {
 
     if (status) {
         res.redirect(`/cubic/details/${req.params.id}`);
+    } else {
+        res.redirect('/error');
     }
 });
 
